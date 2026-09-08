6dcee51586261ca4f8c3e2b5379facdd04363a36
carelessd <poayqt@gmail.com>
2026-09-08 04:14:59 -0500

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: "Test",
-  description: "Writing",
+  title: "我的极简博客",
+  description: "纯粹的文字空间",
   cleanUrls: true,
   themeConfig: {
     sidebar: false,
